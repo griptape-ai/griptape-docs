ff6a8628f26b9668b3629dd659269bfb9ae28d2f
Collin Dutter <collin@griptape.ai>
2024-01-10 11:59:23 -0800

    Parameterize snippts

--- docs/griptape-tools/index.md
+++ docs/griptape-tools/index.md
@@ -2,7 +2,7 @@ Tools give the LLM abilities to invoke outside APIs, reference data sets, and ge
 
 Griptape tools are special Python classes that LLMs can use to accomplish specific goals. Here is an example custom tool for generating a random number:
 
-```python
+```python ignore
 import random
 from griptape.artifacts import TextArtifact
 from griptape.tools import BaseTool
@@ -30,7 +30,7 @@ A tool can have many "activities" as denoted by the `@activity` decorator. Each 
 
 Output artifacts from all tool activities (except for `InfoArtifact` and `ErrorArtifact`) go to short-term `TaskMemory`. To disable that behavior set the `off_prompt` tool parameter to `False`:
 
-```python
+```python ignore
 RandomNumberGenerator(off_prompt=False)
 ```
 
--- tests/integration/test_code_snippets.py
+++ tests/integration/test_code_snippets.py
@@ -1,22 +1,78 @@
 import io
 import pathlib
 import pytest
+import textwrap
 
-from mktestdocs import check_md_file
 
-# Note the use of `str`, makes for pretty output
-@pytest.mark.parametrize('fpath', pathlib.Path("docs").glob("**/*.md"), ids=str)
-def test_code_snippets(fpath, monkeypatch):
+def check_py_string(source):
+    """Exec the python source given in a new module namespace
+
+    Does not return anything, but exceptions raised by the source
+    will propagate out unmodified
+    """
+    try:
+        exec(source, {"__MODULE__": "__main__"})
+    except Exception:
+        print(source)
+        raise
+
+
+def check_codeblock(block, lang="python"):
+    """
+    Cleans the found codeblock and checks if the proglang is correct.
+
+    Returns an empty string if the codeblock is deemed invalid.
+
+    Arguments:
+        block: the code block to analyse
+        lang: if not None, the language that is assigned to the codeblock
+    """
+    first_line = block.split("\n")[0]
+    if lang:
+        if first_line[3:] != lang:
+            return ""
+    return "\n".join(block.split("\n")[1:])
+
+
+def grab_code_blocks(docstring, lang="python"):
+    """
+    Given a docstring, grab all the markdown codeblocks found in docstring.
+
+    Arguments:
+        docstring: the docstring to analyse
+        lang: if not None, the language that is assigned to the codeblock
+    """
+    docstring = textwrap.dedent(docstring)
+    in_block = False
+    block = ""
+    codeblocks = []
+    for idx, line in enumerate(docstring.split("\n")):
+        if line.startswith("```") and "ignore" not in line:
+            if in_block:
+                codeblocks.append(check_codeblock(block, lang=lang))
+                block = ""
+            in_block = not in_block
+        if in_block:
+            block += line + "\n"
+    return [c for c in codeblocks if c != ""]
+
+
+def get_all_code_blocks():
+    return [
+        {"id": f"{fpath.name}-{snippet_num}", "code": snippet}
+        for fpath in pathlib.Path("docs").glob("**/*.md")
+        for snippet_num, snippet in enumerate(grab_code_blocks(fpath.read_text()))
+    ]
+
+
+all_code_blocks = get_all_code_blocks()
+
+
+@pytest.mark.parametrize(
+    "snippet", all_code_blocks, ids=[f["id"] for f in all_code_blocks]
+)
+def test_code_snippet(snippet, monkeypatch):
     # Send some stdin for tests that use the Chat util
-    monkeypatch.setattr('sys.stdin', io.StringIO('Hi\nexit\n'))
+    monkeypatch.setattr("sys.stdin", io.StringIO("Hi\nexit\n"))
 
-    # This example has a code snippet reliant on another snippet.
-    if str(fpath) == 'docs/griptape-tools/index.md':
-        with pytest.raises(NameError):
-            check_md_file(fpath=fpath)
-    # This example requires creating a directory outside of code.
-    elif str(fpath) == 'docs/griptape-tools/custom-tools/index.md':
-        with pytest.raises(ModuleNotFoundError):
-            check_md_file(fpath=fpath)
-    else:
-        check_md_file(fpath=fpath)
+    check_py_string(snippet["code"])
